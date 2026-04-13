c52beaee578315c010fc07b20ca427913e0dd920
arshadhussain810 <arshadhuss.work@gmail.com>
2026-04-13 12:03:30 +0000

    Saving Work

--- src/test/java/com/learning/Employee_crud/Service/EmployeeServiceTest.java
+++ src/test/java/com/learning/Employee_crud/Service/EmployeeServiceTest.java
@@ -1,6 +1,7 @@
 package com.learning.Employee_crud.Service;
 
 import com.learning.Employee_crud.Repository.EmployeeRepository;
+import com.learning.Employee_crud.customException.DuplicateEmployeeException;
 import com.learning.Employee_crud.entity.Employee;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,13 +25,63 @@ class EmployeeServiceTest {
     private EmployeeService employeeService;
 
     @Test
-    void save() {
+    void save_ThrowException_WhenIdNullEmailExists() {
+        Employee mockEmployee = new Employee();
+        mockEmployee.setFirstName("testname");
+        mockEmployee.setLastName("testname");
+        mockEmployee.setEmail("test@example.com");
+        mockEmployee.setDateOfJoining(LocalDate.now());
+        mockEmployee.setStatus("active");
+
+        when(employeeRepository.existsByEmail(mockEmployee.getEmail()))
+                .thenReturn(true);
+
+        assertThrows(DuplicateEmployeeException.class, ()->{
+            employeeService.save(mockEmployee);
+        });
+
+        verify(employeeRepository, never()).save(mockEmployee);
+    }
+
+    @Test
+    void save_SaveEmployee_WhenIdNullEmailDontExist() {
+        Employee mockEmployee = new Employee();
+        mockEmployee.setFirstName("testname");
+        mockEmployee.setLastName("testname");
+        mockEmployee.setEmail("test@example.com");
+        mockEmployee.setDateOfJoining(LocalDate.now());
+        mockEmployee.setStatus("active");
+
+        when(employeeRepository.existsByEmail(mockEmployee.getEmail()))
+                .thenReturn(false);
+
+        employeeService.save(mockEmployee);
+        verify(employeeRepository, times(1)).save(mockEmployee);
+
+    }
+
+    @Test
+    void save_UpdateEmployee_WhenIdNotNullEmaiExist() {
+        Employee mockEmployee = new Employee();
+        mockEmployee.setId(1001);
+        mockEmployee.setFirstName("testname");
+        mockEmployee.setLastName("testname");
+        mockEmployee.setEmail("test@example.com");
+        mockEmployee.setDateOfJoining(LocalDate.now());
+        mockEmployee.setStatus("active");
+
+        when(employeeRepository.existsByEmailAndIdNot(mockEmployee.getEmail(), mockEmployee.getId()))
+                .thenReturn(false);
+
+        employeeService.save(mockEmployee);
+        verify(employeeRepository, times(1)).save(mockEmployee);
+
     }
 
     @Test
     void findById_WhenIdPresent_EmployeeShouldBeReturned() {
         Employee mockEmployee = new Employee();
-        mockEmployee.setId(001);
+        mockEmployee.setId(1001);
         mockEmployee.setFirstName("testname");
         mockEmployee.setLastName("testname");
         mockEmployee.setEmail("test@example.com");
@@ -43,7 +94,7 @@ class EmployeeServiceTest {
         Optional<Employee> result = employeeService.findById(mockEmployee.getId());
 
         assertTrue( result.isPresent());
-        assertEquals(001, result.get().getId());
+        assertEquals(1001, result.get().getId());
 
         verify(employeeRepository, times(1)).findById(mockEmployee.getId());
     }
