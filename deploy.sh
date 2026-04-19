415d21d6f93ad65c3e37b72575febc447f0f772a
Arshad-Hussain8 <arshadhuss.work@gmail.com>
2026-04-19 10:04:53 +0530

    small bug fixes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 1. Build the image
-docker build -t employee-backend:latest .
+docker build -t employee-crud-docker-app:latest .
 
 # 2. Load it into the cluster
-kind load docker-image employee-backend:latest --name kind
+kind load docker-image employee-crud-docker-app:latest --name employee-crud-cluster
 
 # 3. Apply the Kubernetes files
 kubectl apply -f k8s/
--- tunnel.sh
+++ tunnel.sh
@@ -3,4 +3,4 @@
 echo "Connecting to the app..."
 
 # Forwarding the Service is simpler than finding a Pod name
-kubectl port-forward svc/employee-service 8080:8080
+kubectl port-forward svc/employee-service 8080:80
